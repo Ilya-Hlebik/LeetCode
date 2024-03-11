350d400dee7848d03437a4f16e2605a5ed4de9bb
Ilya-Hlebik <felix.2894@mail.ru>
2024-03-10 20:44:10 -0500

    Backtracking - 8 - cont

--- src/Chapter_10_backtracking/lc_08_17_Letter_Combinations_of_a_Phone_Number/Solution.java
+++ src/Chapter_10_backtracking/lc_08_17_Letter_Combinations_of_a_Phone_Number/Solution.java
@@ -1,37 +1,46 @@
 package Chapter_10_backtracking.lc_08_17_Letter_Combinations_of_a_Phone_Number;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Solution {
-    Map<Integer, String> numbers = new HashMap<>();
+    Map<Character, String> numbers = new HashMap<>();
 
     {
-        numbers.put(2, "abc");
-        numbers.put(3, "def");
-        numbers.put(4, "ghi");
-        numbers.put(5, "jkl");
-        numbers.put(6, "mno");
-        numbers.put(7, "pqrs");
-        numbers.put(8, "tuv");
-        numbers.put(9, "wxyz");
+        numbers.put('2', "abc");
+        numbers.put('3', "def");
+        numbers.put('4', "ghi");
+        numbers.put('5', "jkl");
+        numbers.put('6', "mno");
+        numbers.put('7', "pqrs");
+        numbers.put('8', "tuv");
+        numbers.put('9', "wxyz");
     }
 
     public List<String> letterCombinations(String digits) {
-
+        ArrayList<String> result = new ArrayList<>();
+        dfs(digits, result, 0, "");
+        return result;
     }
 
-    public String dfs(List<Integer> digits, List<String> result) {
-
-        for (Integer digit: digits){
-            
+    public void dfs(String digits, List<String> result, int i, String current) {
+        if (current.length() == digits.length()) {
+            if (!current.isEmpty()){
+                result.add(current);
+            }
+            return;
+        }
+        String letters = numbers.get(digits.charAt(i));
+        for (Character digit : letters.toCharArray()) {
+            dfs(digits, result, i+1, current + digit);
         }
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.letterCombinations("23");
+        System.out.println(solution.letterCombinations(""));
     }
 
 }
