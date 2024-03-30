3750a4bed37e9d5967523b781f8599da7357140e
Ilya-Hlebik <felix.2894@mail.ru>
2024-03-29 21:59:15 -0500

    1 D Dynamic - 5

---
+++ src/Chapter_13_1_D_Dynamic_Programming/lc_05_5_Longest_Palindromic_Substring/Solution.java
@@ -0,0 +1,26 @@
+package Chapter_13_1_D_Dynamic_Programming.lc_05_5_Longest_Palindromic_Substring;
+
+public class Solution {
+
+    public String longestPalindrome(String s) {
+        int maxLength = s.length();
+        while (maxLength > 0) {
+            for (int i = 0; i + maxLength - 1 < s.length(); i++) {
+                if (s.charAt(i)!= s.charAt(i + maxLength-1)){
+                  continue;
+                }
+                StringBuilder substring = new StringBuilder(s.substring(i, i + maxLength));
+                if (substring.toString().equals(substring.reverse().toString())) {
+                    return substring.toString();
+                }
+            }
+            maxLength--;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestPalindrome("busislnescsicxpvvysuqgcudefrfjbwwjcchtgqyajdfwvkypfwshnihjdztgmyuuljxgvhdiwphrweyfkbnjgerkmifbirubhseuhrugwrabnjafnbdfjnufdstjbkuwtnpflffaqmjbhssjlnqftgjiglvvequhapasarlkcvbmkwnkuvwktbgfoaxteprobdwswcdyddyvrehvmxrrjiiidatidlpihkbmmruysmhhsncmfdanafdrfpdtfgkglcqpwrrtvacuicohspkounojuziittugpqjyhhkwfnflozbispehrtrnizowrlzcuollagxwtznjwzcumvedjwokueuqktvvouwnsmpxqvvpuwprezrbobrpnwaccwljchdguubjulyilzvmandjjleitweybqkjttschrjjlebnmponvlktzzcdtuybugggcqffkcffpamauvxfbonjrobgpvlyzveiwemmtdvbjciaytvesnocnjrwodtcokgcuoiicxapmrzpkfphjniuvzjrhbnqndfshoduejyktebgdabidxlkstepuwvtrtgbxaeheylicvhrxddijshcvdadxzsccmainyfpfdhqdanfccqkzlmhsfilvoybqojlvbcixjzqpbngdvesuokbxhkomsiqfyukvspqthlzxdnlwthrgaxhtpjzhrugqbfokrdcyurivmzgtynoqfjbafboselxnfupnpqlryvlcxeksirvufepfwczosrrjpudbwqxwldgjyfjhzlzcojxyqjyxxiqvfhjdwtgoqbyeocffnyxhyyiqspnvrpxmrtcnviukrjvpavervvztoxajriuvxqveqsrttjqepvvahywuzwtmgyrzduxfqspeipimyoxmkadrvrdyefekjxcmsmzmtbugyckcbjsrymszftjyllfmoeoylzeahnrxlxpnlvlvzltwnmldi"));
+    }
+}
