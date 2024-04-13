754b4f793fe93529cb6fe73a23fd8b636078a3f5
Ilya-Hlebik <felix.2894@mail.ru>
2024-04-12 20:33:49 -0500

    Greedy - 1

---
+++ src/Chapter_15_Greedy/lc_01_53_Maximum_Subarray/Solution.java
@@ -0,0 +1,19 @@
+package Chapter_15_Greedy.lc_01_53_Maximum_Subarray;
+
+public class Solution {
+
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        for (int num : nums) {
+            currentSum = Math.max(num, currentSum + num);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+}
