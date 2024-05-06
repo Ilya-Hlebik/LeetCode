f419bac3673aa7140a99d1e00ee542e6530b3609
Ilya-Hlebik <felix.2894@mail.ru>
2024-05-05 22:45:14 -0500

    Math and Geo 8 begin

---
+++ src/Chapter_17_Math_and_Geometry/lc_08_2013_Detect_Squares/Solution.java
@@ -0,0 +1,21 @@
+package Chapter_17_Math_and_Geometry.lc_08_2013_Detect_Squares;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
+class DetectSquares {
+
+    public DetectSquares() {
+
+    }
+
+    public void add(int[] point) {
+
+    }
+
+    public int count(int[] point) {
+
+    }
+}
