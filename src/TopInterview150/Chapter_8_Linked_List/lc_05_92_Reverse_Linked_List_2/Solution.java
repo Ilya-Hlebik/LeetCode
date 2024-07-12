b103735a48c9cfa97d5211f064884e7d26ff5c15
Ilya-Hlebik <felix.2894@mail.ru>
2024-07-11 22:47:29 -0500

    Interview - Linked list 5 - end

--- src/TopInterview150/Chapter_8_Linked_List/lc_05_92_Reverse_Linked_List_2/Solution.java
+++ src/TopInterview150/Chapter_8_Linked_List/lc_05_92_Reverse_Linked_List_2/Solution.java
@@ -6,25 +6,41 @@ public class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         ListNode prev = null;
         ListNode current = head;
-        int i =1;
         ListNode oldHead = new ListNode();
-        while (current != null) {
-            if (i<left){
-                oldHead.next = new ListNode(current.val);
+        ListNode tempOldHead = oldHead;
+        ListNode lastPrev = null;
+        for (int i = 1; current != null;i++) {
+            if (i < left) {
+                tempOldHead.next = new ListNode(current.val);
+                tempOldHead = tempOldHead.next;
                 current = current.next;
-            }
-            else if(i<=right) {
+            } else if (i <= right) {
                 ListNode next = current.next;
                 current.next = prev;
                 prev = current;
+                if (lastPrev == null){
+                    lastPrev = prev;
+                }
                 current = next;
-            }
-            else {
+            } else {
                 break;
             }
-            i++;
         }
-        oldHead.next = prev;
+        if (lastPrev!=null){
+            lastPrev.next = current;
+        }
+        tempOldHead.next = prev;
         return oldHead.next;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
+        ListNode x = solution.reverseBetween(node, 2, 4);
+        System.out.println(x);
     }
 }
