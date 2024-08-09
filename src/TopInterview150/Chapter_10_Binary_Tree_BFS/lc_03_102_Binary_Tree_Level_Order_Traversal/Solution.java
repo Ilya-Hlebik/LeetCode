c86ca1619225f5a60b9ba4ad50b176e40dae03c5
Ilya-Hlebik <felix.2894@mail.ru>
2024-08-08 21:32:40 -0500

    Interview -  BTBFS - 3

---
+++ src/TopInterview150/Chapter_10_Binary_Tree_BFS/lc_03_102_Binary_Tree_Level_Order_Traversal/Solution.java
@@ -0,0 +1,35 @@
+package TopInterview150.Chapter_10_Binary_Tree_BFS.lc_03_102_Binary_Tree_Level_Order_Traversal;
+
+import Chapter_7_trees.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null){
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> current = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = queue.poll();
+                current.add(poll.val);
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+            result.add(current);
+        }
+        return result;
+    }
+}
