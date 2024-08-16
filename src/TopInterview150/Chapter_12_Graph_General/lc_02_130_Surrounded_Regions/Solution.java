b1d983bf0362e0f3c3b4e27323b9116bf4f76eef
Ilya-Hlebik <felix.2894@mail.ru>
2024-08-15 23:12:54 -0500

    Interview -  Graph General - 2

--- src/TopInterview150/Chapter_12_Graph_General/lc_02_130_Surrounded_Regions/Solution.java
+++ src/TopInterview150/Chapter_12_Graph_General/lc_02_130_Surrounded_Regions/Solution.java
@@ -4,31 +4,44 @@ public class Solution {
     public void solve(char[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 'O') {
+                if (board[i][j] == 'O' && (i == board.length - 1 || i == 0 || j == 0 || j == board[i].length - 1)) {
                     dfs(board, i, j);
+                }
+            }
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'T') {
+                    board[i][j] = 'O';
                 }
             }
         }
     }
 
-    public boolean dfs(char[][] board, int x, int y) {
-        if (x < 0 || y < 0 || x > board.length - 1 || y > board[x].length - 1 || board[x][y] == 'X') {
-            return true;
+    public void dfs(char[][] board, int x, int y) {
+        if (x < 0 || y < 0 || x > board.length - 1 || y > board[x].length - 1 || board[x][y] != 'O') {
+            return;
         }
-        if ((x == board.length - 1 || x == 0 || y == 0 || y == board[x].length - 1) && board[x][y] != 'X') {
-            return false;
-        }
-        board[x][y] = '-';
-        if ( dfs(board, x - 1, y) &&
-                dfs(board, x, y + 1) &&
-                dfs(board, x + 1, y) &&
-                dfs(board, x, y - 1) ) {
-            board[x][y] = 'X';
-            return true;
-        }
-        else {
-            board[x][y] = 'O';
-            return false;
-        }
+        board[x][y] = 'T';
+        dfs(board, x - 1, y);
+        dfs(board, x, y + 1);
+        dfs(board, x + 1, y);
+        dfs(board, x, y - 1);
+    }
+
+    public static void main(String[] args) {
+        char[][] board = {
+                {'O', 'O', 'O', 'O', 'X', 'X'},
+                {'O', 'O', 'O', 'O', 'O', 'O'},
+                {'O', 'X', 'O', 'X', 'O', 'O'},
+                {'O', 'X', 'O', 'O', 'X', 'O'},
+                {'O', 'X', 'O', 'X', 'O', 'O'},
+                {'O', 'X', 'O', 'O', 'O', 'O'}
+        };
+        Solution solution = new Solution();
+        solution.solve(board);
+        System.out.println();
     }
 }
