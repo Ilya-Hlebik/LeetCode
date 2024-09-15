73dbf222739adc288384b6acd7164326ce334172
Ilya-Hlebik <felix.2894@mail.ru>
2024-09-15 14:46:18 -0500

    Interview 150 -  Dived and Conquer - 1

---
+++ src/TopInterview150/Chapter_15_Divide_And_Conquer/lc_01_108_Convert_Sorted_Array_to_Binary_Search_Tree/Solution.java
@@ -0,0 +1,20 @@
+package TopInterview150.Chapter_15_Divide_And_Conquer.lc_01_108_Convert_Sorted_Array_to_Binary_Search_Tree;
+
+import Chapter_7_trees.TreeNode;
+
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode dfs(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        int mid = start + (end - start) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums, start, mid - 1);
+        root.right = dfs(nums, mid + 1, end);
+        return root;
+    }
+}
