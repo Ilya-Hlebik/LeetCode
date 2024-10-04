2e0731b4391ed978d77e408ea854200936c65613
Ilya-Hlebik <felix.2894@mail.ru>
2024-10-03 22:10:57 -0500

    Interview 150 -  Bit Manipulation - 6 end

--- src/TopInterview150/Chapter_20_Bit_Manipulation/lc_06_201_Bitwise_AND_of_Numbers_Range/Solution.java
+++ src/TopInterview150/Chapter_20_Bit_Manipulation/lc_06_201_Bitwise_AND_of_Numbers_Range/Solution.java
@@ -3,9 +3,13 @@ package TopInterview150.Chapter_20_Bit_Manipulation.lc_06_201_Bitwise_AND_of_Num
 public class Solution {
 
     public int rangeBitwiseAnd(int left, int right) {
-        int ans = left;
-        for (int i = left + 1; i <= right && ans != 0 && i <Integer.MAX_VALUE && i>=0; i++) {
-            ans &= i;
+        int ans = 0;
+        for (int i = 31; i >= 0; i--) {
+            if ((left & (1 << i)) == (right & (1 << i))) {
+                ans |= (left & (1 << i));
+            } else {
+                break;
+            }
         }
         return ans;
     }
