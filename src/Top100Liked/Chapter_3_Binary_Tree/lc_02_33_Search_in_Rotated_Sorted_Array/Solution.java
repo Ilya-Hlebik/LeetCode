da946b0ac71a125a8db208e650cbdd02e77f771a
Ilya-Hlebik <felix.2894@mail.ru>
2024-12-31 16:41:29 -0600

    Top100Liked -  BinarySearch - 2 - finish

--- src/Top100Liked/Chapter_3_Binary_Tree/lc_02_33_Search_in_Rotated_Sorted_Array/Solution.java
+++ src/Top100Liked/Chapter_3_Binary_Tree/lc_02_33_Search_in_Rotated_Sorted_Array/Solution.java
@@ -4,24 +4,24 @@ public class Solution {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        while (right > left) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             int midVal = nums[mid];
             if (midVal == target) {
                 return mid;
             }
-            if (midVal < target && nums[left] < nums[right]) {
-                left = mid + 1;
-            } else if (midVal > target && nums[left] < nums[right]) {
-                right = mid - 1;
-            } else if (midVal < target && nums[left] > nums[right]) {
-                if (nums[right] < target) {
+            if (nums[left] <= midVal) {
+                if (target < midVal && nums[left] <= target) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            } else if (midVal > target && nums[left] > nums[right]) {
-                left = mid + 1;
+            } else {
+                if (target < midVal || nums[right] < target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             }
         }
         return -1;
@@ -29,6 +29,6 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.search(new int[]{4,5,6,7,0,1,2}, 3));
+        System.out.println(solution.search(new int[]{5, 1, 2, 3, 4}, 1));
     }
 }
