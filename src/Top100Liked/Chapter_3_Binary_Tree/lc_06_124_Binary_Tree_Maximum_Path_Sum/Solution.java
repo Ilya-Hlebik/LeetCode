6437457d4c5700b4bef6ac5cb95daba63d625338
Ilya-Hlebik <felix.2894@mail.ru>
2025-01-07 21:50:28 -0600

    Top100Liked -  BinarySearch - 6

---
+++ src/Top100Liked/Chapter_3_Binary_Tree/lc_06_124_Binary_Tree_Maximum_Path_Sum/Solution.java
@@ -0,0 +1,22 @@
+package Top100Liked.Chapter_3_Binary_Tree.lc_06_124_Binary_Tree_Maximum_Path_Sum;
+
+import Chapter_7_trees.TreeNode;
+
+public class Solution {
+    int max = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        return Math.max(dfs(root), max);
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = Math.max(0, dfs(root.left));
+        int val = root.val;
+        int right = Math.max(0, dfs(root.right));
+        max = Math.max(max, val + right + left);
+        return val + Math.max(left, right);
+    }
+}
