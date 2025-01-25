df9840c2d770f046023a7659f0d4a1595f72df31
Ilya-Hlebik <felix.2894@mail.ru>
2025-01-24 20:18:55 -0600

    Top100Liked -  DP - 2

--- src/Top100Liked/Chapter_4_Dynamic_Programming/lc_02_32_Longest_Valid_Parentheses/Solution.java
+++ src/Top100Liked/Chapter_4_Dynamic_Programming/lc_02_32_Longest_Valid_Parentheses/Solution.java
@@ -1,23 +1,23 @@
 package Top100Liked.Chapter_4_Dynamic_Programming.lc_02_32_Longest_Valid_Parentheses;
 
+import java.util.LinkedList;
+
 public class Solution {
     public int longestValidParentheses(String s) {
-        int newSize;
-        int temp;
         int max = 0;
-        for (int i = 2; i <= s.length(); i += 2) {
-            for (int j = 0, k = i; k <= s.length() && max < i; j++, k++) {
-                String substring = s.substring(j, k);
-                newSize = substring.length();
-                temp = 0;
-                while (temp != newSize && newSize != 0) {
-                    temp = newSize;
-                    substring = substring.replace("()", "");
-                    newSize = substring.length();
-                }
-                if (newSize == 0) {
-                    max = i;
-                    break;
+        int left = -1;
+        LinkedList<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else if (stack.isEmpty()) {
+                left = i;
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    max = Math.max(max, i - left);
+                } else {
+                    max = Math.max(max, i - stack.peek());
                 }
             }
         }
@@ -26,6 +26,6 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.longestValidParentheses(")()())");
+        System.out.println(solution.longestValidParentheses("()(())"));
     }
 }
