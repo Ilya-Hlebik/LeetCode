4c5a5659f08549cf985e7908f537cc4cea58df5c
Ilya-Hlebik <felix.2894@mail.ru>
2025-03-02 17:53:36 -0600

    Top100Liked -  Heap - 1

---
+++ src/Top100Liked/Chapter_8_Heap/lc_01_215_Kth_Largest_Element_in_an_Array/Solution.java
@@ -0,0 +1,16 @@
+package Top100Liked.Chapter_8_Heap.lc_01_215_Kth_Largest_Element_in_an_Array;
+
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> Integer.compare(o2,o1));
+        for (int num : nums) {
+            queue.add(num);
+            if (queue.size()>nums.length-1){
+                queue.poll();
+            }
+        }
+        return queue.poll();
+    }
+}
