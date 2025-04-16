10af07fa951c5ff942d258e3570888ec7e53c9f3
Ilya-Hlebik <felix.2894@mail.ru>
2025-04-15 21:45:13 -0500

    Top100Liked -  Sliding window - 1

---
+++ src/Top100Liked/Chapter_13_Sliding_Window/lc_01_11_Container_With_Most_Water/Solution.java
@@ -0,0 +1,23 @@
+package Top100Liked.Chapter_13_Sliding_Window.lc_01_11_Container_With_Most_Water;
+
+public class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int max = 0;
+        while (l < r && r < height.length) {
+            max = Math.max(max, Math.min(height[l], height[r]) * (r - l));
+            if (height[r] > height[l]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maxArea(new int[]{1,1}));
+    }
+}
