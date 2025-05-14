486effee52e913cb67668f58c1898a3d554c6a89
Ilya-Hlebik <felix.2894@mail.ru>
2025-05-13 22:25:06 -0500

    Free run - 5 - sol 1

---
+++ src/freeRun/lc_05_29_Divide_Two_Integers/Solution.java
@@ -0,0 +1,26 @@
+package freeRun.lc_05_29_Divide_Two_Integers;
+
+public class Solution {
+    public int divide(int dividend, long divisor) {
+        int c = dividend < 0 && divisor > 0 || dividend >= 0 && divisor < 0 ? -1 : 1;
+        long dd = Math.abs((long) dividend);
+        long dr = Math.abs((long)divisor);
+        if (divisor == 1){
+            return dividend;
+        }
+        long res = 0;
+        for (int i = 30; i >= 0; i--) {
+            long temp =  dr << i ;
+            if (temp <= dd) {
+                res += (1<<i);
+                dd-=temp;
+            }
+        }
+        return (int) (c < 0 ? -res : res);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.divide(-2147483648, -1));
+    }
+}
