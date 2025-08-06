c3d2bac361acdf9f176b5efb65632fe01c91e2c0
Ilya-Hlebik <felix.2894@mail.ru>
2025-08-06 11:04:27 -0500

    Free run - 31

--- src/freeRun/lc_31_166_Fraction_to_Recurring_Decimal/Solution.java
+++ src/freeRun/lc_31_166_Fraction_to_Recurring_Decimal/Solution.java
@@ -1,25 +1,56 @@
 package freeRun.lc_31_166_Fraction_to_Recurring_Decimal;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
-        double devide = (double) numerator / denominator;
-        String temp = String.valueOf(devide);
         StringBuilder result = new StringBuilder();
-        String split[] = temp.split("\\.");
-        if (split[1].equals("0")) {
-            return split[0];
+        long numeratorLong = numerator;
+        long denominatorLong = denominator;
+        if (numeratorLong < 0 & denominatorLong > 0 || numeratorLong > 0 && denominatorLong < 0) {
+            result.append("-");
+            numeratorLong = Math.abs(numeratorLong);
+            denominatorLong = Math.abs(denominatorLong);
         }
-
-
-
+        result.append(numeratorLong / denominatorLong);
+        long remainder = numeratorLong % denominatorLong;
+        if (remainder == 0) {
+            return result.toString();
+        }
+        result.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        List<String> list = new ArrayList<>();
         int i = 0;
-        while (temp.charAt(i++) != '.') {
+        while (remainder != 0) {
+            if (map.containsKey(remainder)){
+                Integer index = map.get(remainder);
+                for (int j = 0; j < list.size(); j++) {
+                    if (index == j){
+                        result.append("(");
+                    }
+                    result.append(list.get(j));
+                }
+                result.append(")");
+                return result.toString();
+            }
+            else {
+                map.put(remainder, i++);
+                remainder *= 10;
+                long nextDigit = remainder / denominatorLong;
+                remainder %= denominatorLong;
+                list.add(String.valueOf(nextDigit));
+            }
         }
-        return temp;
+        result.append(String.join("", list));
+        return result.toString();
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.fractionToDecimal(2, 1));
+        System.out.println(solution.fractionToDecimal(-2147483648
+                , 1));
     }
 }
