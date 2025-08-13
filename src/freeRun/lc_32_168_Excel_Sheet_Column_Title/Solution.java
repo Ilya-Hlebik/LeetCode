b5343f8239514e35d69070134930764e041e2a9a
Ilya-Hlebik <felix.2894@mail.ru>
2025-08-12 22:20:03 -0500

    Free run - 32

--- src/freeRun/lc_32_168_Excel_Sheet_Column_Title/Solution.java
+++ src/freeRun/lc_32_168_Excel_Sheet_Column_Title/Solution.java
@@ -1,26 +1,18 @@
 package freeRun.lc_32_168_Excel_Sheet_Column_Title;
 
-import java.util.Stack;
-
 public class Solution {
     public String convertToTitle(int columnNumber) {
         StringBuilder result = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
-        if (columnNumber <= 26) {
-            return Character.toString('A' + columnNumber - 1);
-        }
-        while (columnNumber > 26) {
+
+        while (columnNumber > 0) {
             int remainder = columnNumber % 26;
-            stack.push(remainder);
-            columnNumber /= 26;
-            if (columnNumber<=26) {
-                stack.push(columnNumber);
+            result.append((char)('A' + (remainder == 0 ? 26 : remainder) - 1));
+            if (remainder == 0) {
+                columnNumber -= 1;
             }
+            columnNumber /= 26;
         }
-        while (!stack.isEmpty()) {
-            result.append(Character.toString('A' + stack.pop() - 1));
-        }
-        return result.toString();
+        return result.reverse().toString();
     }
 
     public static void main(String[] args) {
