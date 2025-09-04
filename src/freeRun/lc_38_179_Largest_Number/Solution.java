6ebdfe9129a88bf90e2c9a0694ac27763c3f1bb9
Ilya-Hlebik <felix.2894@mail.ru>
2025-09-03 21:26:24 -0500

    Free run - 38

--- src/freeRun/lc_38_179_Largest_Number/Solution.java
+++ src/freeRun/lc_38_179_Largest_Number/Solution.java
@@ -5,43 +5,14 @@ import java.util.stream.Collectors;
 
 public class Solution {
     public String largestNumber(int[] nums) {
-        String collect = Arrays.stream(nums).boxed().map(Object::toString)
-                .sorted((o1, o2) -> {
-                    char[] charArray1 = o1.toCharArray();
-                    char[] charArray2 = o2.toCharArray();
-                    for (int i = 0; i < Math.max(charArray1.length, charArray2.length); i++) {
-                        if (i < charArray1.length && i < charArray2.length) {
-                            int compare = Integer.compare(Character.digit(charArray2[i], 10), Character.digit(charArray1[i], 10));
-                            if (compare != 0) {
-                                return compare;
-                            }
-                        } else if (i < charArray1.length) {
-                            for (int j = 0; j < charArray2.length; j++) {
-                                int compare = Integer.compare(Character.digit(charArray2[j], 10), Character.digit(charArray1[i], 10));
-                                if (compare != 0) {
-                                    return compare;
-                                }
-                            }
-                        } else {
-                            for (int j = 0; j < charArray1.length; j++) {
-                                int compare = Integer.compare(Character.digit(charArray2[i], 10), Character.digit(charArray1[j], 10));
-                                if (compare != 0) {
-                                    return compare;
-                                }
-                            }
-                        }
-                    }
-                    return 0;
-                })
+        String result = Arrays.stream(nums).boxed().map(Object::toString)
+                .sorted((o1, o2) -> (o2 + o1).compareTo(o1 + o2))
                 .collect(Collectors.joining());
-        while (collect.length() > 1 && collect.charAt(0) == '0') {
-           collect = collect.substring(1);
-        }
-        return collect;
+        return result.charAt(0) =='0'? "0": result;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.largestNumber(new int[]{92, 923}));
+        System.out.println(solution.largestNumber(new int[]{12341, 123411234}));
     }
 }
