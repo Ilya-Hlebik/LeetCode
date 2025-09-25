ef12e117321ad7579f7efc6d68bf4f165c3884ed
Ilya-Hlebik <felix.2894@mail.ru>
2025-09-25 10:28:13 -0500

    Free run - 47

--- src/freeRun/lc_47_204_Count_Primes/Solution.java
+++ src/freeRun/lc_47_204_Count_Primes/Solution.java
@@ -1,23 +1,19 @@
 package freeRun.lc_47_204_Count_Primes;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public class Solution {
     public int countPrimes(int n) {
-        Map<Integer, Boolean> map = new LinkedHashMap<>();
-        for (int i =2; i< n; i++){
-            map.put(i, true);
-        }
-        for (Map.Entry<Integer, Boolean> el : map.entrySet()) {
-            if (Math.pow(el.getKey(), 2)>n){
-                break;
+        boolean[] map = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < map.length; i++) {
+            if (map[i]) {
+                continue;
             }
-            for (int i = el.getKey() + el.getKey(); i< n; i+=el.getKey()){
-                map.put(i, false);
+            count++;
+            for (long j = (long) i * i; j < n; j += i) {
+                map[(int) j] = true;
             }
         }
-        return (int) map.entrySet().stream().filter(Map.Entry::getValue).count();
+        return count;
     }
 
     public static void main(String[] args) {
