9dc86f24345abed18a3779f40ed834714c8be625
Ilya-Hlebik <felix.2894@mail.ru>
2025-11-13 23:11:02 -0600

    Free run - 62 Initial

---
+++ src/freeRun/lc_62_273_Integer_to_English_Words/Solution.java
@@ -0,0 +1,13 @@
+package freeRun.lc_62_273_Integer_to_English_Words;
+
+public class Solution {
+    String numbers[] = new String[]{"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    String tens[] = new String[]{"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String big[] = new String[]{"", "", "Hundred", "Thousand", "Million"};
+
+    public String numberToWords(int num) {
+
+        while ()
+    }
+}
