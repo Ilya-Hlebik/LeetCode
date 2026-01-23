c0da0ea4d804b7f3f7cdd233cd73cac2bd1db5ce
Ilya-Hlebik <felix.2894@mail.ru>
2026-01-22 21:28:53 -0600

    Free run - 73 part 2

--- src/freeRun/lc_73_306_Additive_Number/Solution.java
+++ src/freeRun/lc_73_306_Additive_Number/Solution.java
@@ -1,22 +1,35 @@
 package freeRun.lc_73_306_Additive_Number;
 
+import java.math.BigInteger;
+
 public class Solution {
     public boolean isAdditiveNumber(String num) {
-        return dfs(num, 1, Integer.parseInt(num.substring(0, 1)));
-    }
-
-    private boolean dfs(String num, int index, int previous) {
-        if (index+1 == num.length()){
-            return true;
+        if (num.length() < 3) {
+            return false;
         }
-        while (num.charAt(index) =='0'){
-            index++;
-        }
-        for (int i = index; i < num.length(); i++) {
-
-            int next = Integer.parseInt(num.substring(index, i+1));
-            if (num.substring(i+1).startsWith(String.valueOf(previous + next))) {
-              return   dfs(num, i+1, next);
+        for (int i = 1; i < num.length() - 1; i++) {
+            if (num.charAt(0) == '0' && i > 1) break;
+            String first = num.substring(0, i);
+            for (int j = i + 1; j < num.length(); j++) {
+                String second = num.substring(i, j);
+                if (second.length() > 1 && second.charAt(0) == '0') {
+                    break;
+                }
+                BigInteger a = new BigInteger(first);
+                BigInteger b = new BigInteger(second);
+                for (int index = j; index < num.length(); ) {
+                    String sum = String.valueOf(a.add(b));
+                    if (num.substring(index).startsWith(sum)) {
+                        index += sum.length();
+                        if (index == num.length()) {
+                            return true;
+                        }
+                        a = b;
+                        b = new BigInteger(sum);
+                    } else {
+                        break;
+                    }
+                }
             }
         }
         return false;
@@ -24,6 +37,6 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.isAdditiveNumber("199100199"));
+        System.out.println(solution.isAdditiveNumber("999999999999999999999999"));
     }
 }
