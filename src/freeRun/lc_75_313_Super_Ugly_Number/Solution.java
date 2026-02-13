e4b66c7668a0145fd599649ea77c7924a1b450c4
Ilya-Hlebik <felix.2894@mail.ru>
2026-02-12 21:19:27 -0600

    Free run - 75

--- src/freeRun/lc_75_313_Super_Ugly_Number/Solution.java
+++ src/freeRun/lc_75_313_Super_Ugly_Number/Solution.java
@@ -1,28 +1,25 @@
 package freeRun.lc_75_313_Super_Ugly_Number;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
 public class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
         int[] pointers = new int[primes.length];
         int[] uglyNumbers = new int[n];
         uglyNumbers[0] = 1;
+        int uglyIndex = 1;
         while (uglyNumbers[n - 1] == 0) {
-            List<int[]> list = new ArrayList<>();
+            int min = Integer.MAX_VALUE;
+
             for (int i = 0; i < primes.length; i++) {
                 int prime = primes[i];
                 int candidate = prime * uglyNumbers[pointers[i]];
-                list.add(new int[]{candidate, i});
+                if (candidate > 0) {
+                    min = Math.min(candidate, min);
+                }
             }
-            list.sort(Comparator.comparingInt(o -> o[0]));
-            int min = list.get(0)[0];
-            for (int[] ints : list) {
-                if (ints[0] == min) {
-                    pointers[ints[1]]++;
-                } else {
-                    break;
+            uglyNumbers[uglyIndex++] = min;
+            for (int i = 0; i < pointers.length; i++) {
+                if (primes[i] * uglyNumbers[pointers[i]] == min) {
+                    pointers[i]++;
                 }
             }
         }
@@ -31,6 +28,6 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.nthSuperUglyNumber(12, new int[]{2,7,13,19});
+        System.out.println(solution.nthSuperUglyNumber(5911, new int[]{2, 3, 5, 7}));
     }
 }
